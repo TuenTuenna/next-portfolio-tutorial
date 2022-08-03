f5692bf1c86c35c2153e355d4dca586e350432ab
Dev-JeongDaeRi <kor216465@gmail.com>
2022-08-03 12:38:16 +0900

    revalidate 에 대한 내용 추가

--- pages/projects.js
+++ pages/projects.js
@@ -36,7 +36,6 @@ export default function Projects({projects}) {
 // 각 요청 때마다 호출
 export async function getServerSideProps() {
 
-
     const options = {
         method: 'POST',
         headers: {
@@ -68,5 +67,7 @@ export async function getServerSideProps() {
 
     return {
       props: {projects}, // will be passed to the page component as props
+      // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
+      // revalidate: 1 // 데이터 변경이 있으면 갱신 1초 마다
     }
 }
